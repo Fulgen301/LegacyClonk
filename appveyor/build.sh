f0478ec6fa5f70bc0bd5488a322fc3a27ab4db7f
Jan Heberer <jangc@t-online.de>
2019-09-17 18:41:37 +0200

    Use Docker instead of chroot to build i386 Linux version
    Also clean up the build script:
    * Exit on error and print executed commands
    * "apt-get install": only install required packages, use "--no-install-recommends", sort package list alphabetically and move options to the front
    * Set g++/gcc version in CMake instead of changing it globally

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-sudo DEBIAN_FRONTEND=noninteractive apt-get install zlib1g-dev build-essential libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl1.2-dev libsdl-mixer1.2-dev libfreetype6-dev freeglut3-dev libxpm-dev cmake make git -y
-sudo update-alternatives --set gcc /usr/bin/gcc-9
-git status || cd /legacyclonk
+
+set -ex
+
+readonly GCC=gcc-9
+readonly GXX=g++-9
+
+export readonly DEBIAN_FRONTEND=noninteractive
+
+# Use sudo if it is installed
+sudo=$(hash sudo 2> /dev/null && echo sudo || echo)
+
+# Prepare package installation
+$sudo sed -i 's/\bhttp:\/\/archive.ubuntu.com\//http:\/\/us-central1.gce.archive.ubuntu.com\//g' /etc/apt/sources.list
+$sudo apt-get update
+install="$sudo apt-get --no-install-recommends -y install"
+
+# Install gcc and g++. Use PPA if normal installation fails.
+if ! $install $GCC $GXX ; then
+	$install software-properties-common
+	$sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+	$install $GCC $GXX
+fi
+
+# Install remaining packages
+$install cmake freeglut3-dev git libc-dev libfreetype6-dev libgl1-mesa-dev libglew-dev libglu1-mesa-dev libjpeg-dev libpng-dev libsdl-mixer1.2-dev libsdl1.2-dev libssl-dev libxpm-dev make zlib1g-dev
+
+# Run CMake and build project
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cmake -DCMAKE_C_COMPILER=$GCC -DCMAKE_CXX_COMPILER=$GXX -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 make -j$(nproc)
-test -e clonk && tar -cvzf LegacyClonk-${PLATFORM}.tar.gz clonk c4group
+tar -cvzf LegacyClonk.tar.gz clonk c4group
