4f8fa760e8aa995405f53fc269cf645595285310
Fulgen301 <tokmajigeorge@gmail.com>
2020-03-26 15:13:41 +0100

    SetShaderProgramForMode: Unset script shader if nil is passed as name

--- src/C4Script.cpp
+++ src/C4Script.cpp
@@ -6456,6 +6456,12 @@ static bool FnSetShaderProgramForMode(C4AulContext *ctx, long mode, C4String *na
 		return false;
 	}
 
+	if (!name)
+	{
+		lpDDraw->SetShaderProgramForMode(static_cast<CStdDDraw::DrawMode>(mode), nullptr);
+		return true;
+	}
+
 	if (auto it = Game.ScriptShader.find(FnStringPar(name)); it != Game.ScriptShader.end())
 	{
 		lpDDraw->SetShaderProgramForMode(static_cast<CStdDDraw::DrawMode>(mode), it->second.get());
