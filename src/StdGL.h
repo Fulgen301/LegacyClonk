3dcebf9162d298c15c060b9cfb66b8c2c238b10d
Fulgen301 <tokmajigeorge@gmail.com>
2020-04-02 12:25:18 +0200

    Fix compile errors

--- src/StdGL.h
+++ src/StdGL.h
@@ -18,6 +18,8 @@
 /* OpenGL implementation of NewGfx */
 
 #pragma once
+
+#include <Standard.h>
 #ifdef USE_GL
 
 #include <GL/glew.h>
@@ -138,9 +140,9 @@ public:
 	bool Select(bool verbose = false) override; // select this context
 	void Deselect(); // select this context
 	void Destroy(); // destroy this context
-	bool UpdateSize(); // get new size from hWnd
+	bool UpdateSize() override; // get new size from hWnd
 
-	bool PageFlip(); // present scene
+	bool PageFlip() override; // present scene
 
 protected:
 	bool CreateContext(int major, int minor, bool core = true);
