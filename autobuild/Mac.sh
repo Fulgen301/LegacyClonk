4e6c50a08e17ee0a563e5029f676cb4d178e95ab
George Tokmaji <tokmajigeorge@gmail.com>
2022-11-08 13:22:37 +0100

    Mac: use gcc

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Wno-parentheses -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" >> $GITHUB_ENV
+echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_C_COMPILER=/usr/local/bin/gcc-12 -DCMAKE_CXX_COMPILER=/usr/local/bin/g++-12 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Wno-parentheses -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" >> $GITHUB_ENV
 
 cat autobuild/mac_any_cast.h >> src/C4InteractiveThread.h
